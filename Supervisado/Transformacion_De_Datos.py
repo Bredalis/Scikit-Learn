586cc2bdbf1cd6d19c26a868204deca915710a6e
PRO X <bredalisgautreaux@gmail.com>
2023-12-23 21:23:37 -0400

    Se modifico un archivo

--- Supervisado/Transformacion_De_Datos.py
+++ Supervisado/Transformacion_De_Datos.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 # Lectura de datos
-
 df = pd.read_csv("C:/Users/Angelica Gerrero/Desktop/LenguajesDeProgramacion/Datasets/CSV/Datos.csv")
 print(f"Dataset: \n{df}")
 
@@ -16,18 +15,14 @@ datos = datos.astype(float).fillna(0.0)
 print(f"Datos (Columna - likes): \n{datos}")
 
 # Converir los datos a una matriz
-
 matriz = np.array(datos).reshape(-1, 1)
-
 print(f"\nDatos convertidos a matriz: \n{matriz}")
 
 # Escaladores
-
 escalador_1 = preprocessing.RobustScaler()
 escalador_2 = preprocessing.StandardScaler()
 
 # Entrenamiento
-
 columna = escalador_1.fit_transform(matriz)
 df_1 = pd.DataFrame(columna)
 
@@ -35,23 +30,19 @@ columna = escalador_2.fit_transform(matriz)
 df_2 = pd.DataFrame(columna)
 
 # Poner nombres a las columnas
-
 df_1.columns = ["RobustScaler"]
 df_2.columns = ["StandardScaler"]
 
 # Mostrar df
-
 print(f"\nDF 1: \n{df_1}")
 print(f"\nDF 2: \n{df_2}")
 
 # Mostra datos de los df
-
 print(f"\nCantidad de la matriz: {matriz.shape}")
 print(f"\nDescripcion DF 1: \n{df_1.describe()}")
 print(f"\nDescripcion DF 2: \n{df_2.describe()}")
 
 # Grafica
-
 datos.plot.kde(legend = True)
 ejes = df_1.plot.kde(legend = True)
 df_2.plot.kde(legend = True, ax = ejes)
